09ce196d24be80030d34897efa76376d9742cd1e
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2020-01-25 00:58:11 +1100

    Updated arena generation

--- Assets/Scripts/ArenaGeneration.cs
+++ Assets/Scripts/ArenaGeneration.cs
@@ -4,73 +4,83 @@ using UnityEngine;
 
 public class ArenaGeneration : MonoBehaviour
 {
-    public Vector3Int maxArenaSize = new Vector3Int(10,3,10);
-    public Vector2Int emptyBlockChance = new Vector2Int(18,24);
+    public Vector2Int maxArenaSize = new Vector2Int(10, 10);
+    public Vector2Int emptyBlockChance = new Vector2Int(18, 24);
+    public Vector2Int wallChance = new Vector2Int(2, 24);
 
     public List<Block> blocks = new List<Block>();
+    public List<GameObject> walls = new List<GameObject>();
     public LayerMask blockLayer;
 
     public int outerRimFraction = 8;
     public int outerRimChance = 2;
     public int cornerChance = 5;
-    
+
     void Start()
     {
         StartCoroutine("GenerateArena");
     }
 
-    IEnumerator GenerateArena ()
+    IEnumerator GenerateArena()
     {
         int lowRimX = maxArenaSize.x / outerRimFraction;
         int highRimX = maxArenaSize.x - lowRimX;
 
-        int lowRimZ = maxArenaSize.z / outerRimFraction;
-        int highRimZ = maxArenaSize.z - lowRimZ;
+        int lowRimZ = maxArenaSize.y / outerRimFraction;
+        int highRimZ = maxArenaSize.y - lowRimZ;
 
         int chance = emptyBlockChance.x;
-        
 
-        for (float y = 0; y < maxArenaSize.y; y += 0.5f)
+
+        for (int z = 0; z < maxArenaSize.y; z++)
         {
-            for (int z = 0; z < maxArenaSize.z; z++)
+            for (int x = 0; x < maxArenaSize.x; x++)
             {
-                for (int x = 0; x < maxArenaSize.x; x++)
+                //Corners
+                if ((x < lowRimX || x > highRimX) && (z < lowRimZ || z > highRimZ))
                 {
-                    //Corners
-                    if ((x < lowRimX || x > highRimX) && (z < lowRimZ || z > highRimZ))
-                    {
-                        chance = (emptyBlockChance.x / cornerChance);
-                    }
-                    // Edges
-                    if ((x < lowRimX || x > highRimX) || (z < lowRimZ || z > highRimZ))
-                    {
-                        chance = (emptyBlockChance.x / outerRimChance);
-                    }
-                    // Centre
-                    else
-                    {
-                        chance = emptyBlockChance.x;
-                    }
-                    
-                    transform.position = new Vector3(x,y,z);
-                    
-                    int randomNumber = Random.Range(0,emptyBlockChance.y);
-                    if (randomNumber < chance)
-                    {
-                        if (blocks != null)
-                        {
-                            if (Physics.Raycast(transform.position,Vector3.down,1f,blockLayer) || y == 0)
-                            {
-                                Instantiate(blocks[Random.Range(0, blocks.Count)], transform.position, Quaternion.identity);                           
-
-                            }                                            
-                        }
-                    }
+                    chance = (emptyBlockChance.x / cornerChance);
                 }
-                    yield return null;
+                // Edges
+                if ((x < lowRimX || x > highRimX) || (z < lowRimZ || z > highRimZ))
+                {
+                    chance = (emptyBlockChance.x / outerRimChance);
+                }
+                // Centre
+                else
+                {
+                    chance = emptyBlockChance.x;
+                }
+
+                transform.position = new Vector3(x, 0, z);
+
+                int randomNumber = Random.Range(0, emptyBlockChance.y);
+                if (randomNumber < chance)
+                {
+                    if (blocks != null)
+                    {
+                        Instantiate(blocks[Random.Range(0, blocks.Count)], transform.position, Quaternion.identity);
+
+                    }
+
+                }
+                yield return null;
             }
-            
-            emptyBlockChance = new Vector2Int(emptyBlockChance.x - 2 ,emptyBlockChance.y);
+        }
+
+        for (int z = 0; z < maxArenaSize.y; z++)
+        {
+            for (int x = 0; x < maxArenaSize.x; x++)
+            {
+                transform.position = new Vector3(x, 1, z);
+                int randomNumber = Random.Range(0, wallChance.y);
+                if (randomNumber < chance)
+                {
+                    Instantiate(walls[Random.Range(0, walls.Count)], transform.position, Quaternion.identity);
+
+                }
+                yield return null;
+            }
         }
     }
 }
